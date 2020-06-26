2bb478fdbd7762518550f05a7def59e1385bcc4e
Fernando Marques <fernando.silva@ccc.ufcg.edu.br>
2020-06-25 23:31:47 -0300

    Criando função de excluir no frontend

--- web/src/service/api.js
+++ web/src/service/api.js
@@ -5,8 +5,14 @@ const index = async ( ) => {
     return await axios.get("http://localhost:3333");
 }
 
+const remove = async ( id ) => {
+    console.log(id);
+    return await axios.delete(`http://localhost:3333/excluir/${id}`);
+}
+
 
 
 export default {
-    index
+    index,
+    remove
 }
